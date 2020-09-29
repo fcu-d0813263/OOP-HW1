1ad883070657edf29ebdef08e8355cba693cab54
user <user@ED08>
2020-09-29 09:12:04 +0800

    46

--- src/Lab01.java
+++ src/Lab01.java
@@ -3,7 +3,7 @@ public class Lab01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello!!");
+		System.out.println("fangci stupid. bagayarooooooooooooo");
 	}
 
 }
